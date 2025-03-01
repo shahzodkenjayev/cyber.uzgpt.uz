0159478666b0ed00901c569532a6d2c740ad0369
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-03-02 03:24:00 +0500

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://cyber.uzgpt.uz/favicon.ico" type="image/x-icon">
     <title><?= $site_name; ?></title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
