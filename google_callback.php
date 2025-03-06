91679b9adc07e98f6a742dfc8cfe74a7a06c9ade
shahzodkenjayev <58332373+shahzodkenjayev@users.noreply.github.com>
2025-03-06 20:39:03 +0500

    Update google_callback.php

--- google_callback.php
+++ google_callback.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
 require __DIR__ . '/config/config.php'; // Ma'lumotlar bazasi ulanishi
 
 $client_id = getenv('GOOGLE_CLIENT_ID');
